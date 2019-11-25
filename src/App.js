7e1802c23c3b39d7a059a8b6376a467d9630191c
Mikulew <mikulew@gmail.com>
2019-11-25 23:01:39 +0100

    Installing react-router-dom, adding Header and views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Docs from './views/Docs.js';
+import Tips from './views/Tips.js';
+import Patterns from './views/Patterns.js';
+import Components from './views/Components.js';
 import styles from './App.module.scss';
-import ItemList from './components/ItemsList/ItemsList';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <ItemList />
-    </div>
+    <Router>
+      <Header />
+      <div className={styles.app}>
+        <Route path="/" exact component={Docs} />
+        <Route path="/tips" exact component={Tips} />
+        <Route path="/patterns" exact component={Patterns} />
+        <Route path="/components" exact component={Components} />
+      </div>
+    </Router>
   );
 }
 
