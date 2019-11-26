b5aa4a0f50ef19ffce4e1a61eed138a8dcb7a33d
Mikulew <mikulew@gmail.com>
2019-11-26 14:43:11 +0100

    Removing js extension

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Docs from './views/Docs.js';
-import Tips from './views/Tips.js';
-import Patterns from './views/Patterns.js';
-import Components from './views/Components.js';
+import Docs from './views/Docs';
+import Tips from './views/Tips';
+import Patterns from './views/Patterns';
+import Components from './views/Components';
 import styles from './App.module.scss';
 
 function App() {
