9cd524782fa2259194d6806681a4b84410e5b3b0
Mikulew <mikulew@gmail.com>
2019-11-28 17:16:28 +0100

    Installing gsap and using useRef

--- src/views/Components.js
+++ src/views/Components.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import styles from './Components.module.scss';
 
-const Components = () => (
-  <div>
-    <h2 className="title is-3">Components</h2>
-  </div>
-);
+const Components = () => {
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(boxRef.current, { x: '-100%', opasity: 0, scale: 5 });
+  });
+
+  return (
+    <div>
+      <h2 className="title is-3">Components</h2>
+      <div ref={boxRef} className={styles.box} />
+    </div>
+  );
+};
 
 export default Components;
