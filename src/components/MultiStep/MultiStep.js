bf05cfe72fa2a341d5a9085f2bea76cfee40dcd4
Mikulew <mikulew@gmail.com>
2019-12-16 22:43:39 +0100

    Making MultiStep compound component

--- src/components/MultiStep/MultiStep.js
+++ src/components/MultiStep/MultiStep.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+export const Page = ({ children }) => (<div>{children}</div>);
+
+export const Controls = () => (
+  <div>
+    <button type="button">Previous</button>
+    <button type="button">Next</button>
+    {/* <button>Submit</button> */}
+  </div>
+);
+
 const MultiStep = ({ children }) => {
   return (
     <div>
--- src/views/Users.js
+++ src/views/Users.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import MultiStep from '../components/MultiStep/MultiStep';
+import MultiStep, { Page, Controls } from '../components/MultiStep/MultiStep';
 
 const Users = () => (
   <div>
     <h2 className="title is-3">Users</h2>
     <MultiStep>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, mollitia?</p>
+      <Page>Page 1</Page>
+      <Page>Page 2</Page>
+      <Page>Page 3</Page>
+      <Controls />
     </MultiStep>
   </div>
 );
