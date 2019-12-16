7ee7d17ea6a7221af16f82986a178507ee923d10
Mikulew <mikulew@gmail.com>
2019-12-16 22:37:47 +0100

    Adding MultiStep

--- src/views/Users.js
+++ src/views/Users.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import MultiStep from '../components/MultiStep/MultiStep';
 
 const Users = () => (
   <div>
     <h2 className="title is-3">Users</h2>
+    <MultiStep>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, mollitia?</p>
+    </MultiStep>
   </div>
 );
 
